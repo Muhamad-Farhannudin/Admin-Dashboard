fb718475ea01912f37c3c93294f5fe0ba74dc076
Muhamad-Farhannudin <110012265+Muhamad-Farhannudin@users.noreply.github.com>
2023-01-18 20:30:02 +0700

    memperbaiki deploy

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
   
   return (
     <>
-      <Routes>
+      <Routes path='/'>
         <Route path='/' element={<Home />}></Route>
         <Route path='/login' element={<Login />}></Route>
         <Route path='/daftar' element={<Daftar />}></Route>
