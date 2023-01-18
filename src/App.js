eca7b16cb1808903b2f1a887c73fe41231f1c256
Muhamad-Farhannudin <110012265+Muhamad-Farhannudin@users.noreply.github.com>
2023-01-18 20:24:57 +0700

    Redeploy pages

--- src/App.js
+++ src/App.js
@@ -2,8 +2,7 @@ import Home from './Pages/Home'
 import { Route, Routes } from 'react-router-dom';
 import Login from './Pages/Login';
 import Daftar from './Pages/Daftar';
-import Sidebar from './components/Sidebar';
-
+// import Sidebar from './components/Sidebar';
 
 function App() {
   
@@ -13,7 +12,7 @@ function App() {
         <Route path='/' element={<Home />}></Route>
         <Route path='/login' element={<Login />}></Route>
         <Route path='/daftar' element={<Daftar />}></Route>
-        <Route path='/sideBar' element={<Sidebar />}></Route>
+        {/* <Route path='/sideBar' element={<Sidebar />}></Route> */}
       </Routes>
     </>
   );
